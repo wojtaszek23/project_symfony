4a409c04db8008644b027fdc159dd9b5c38fbda3
wojtaszek23 <wojtaszek23@wp.pl>
2024-04-04 16:29:09 +0200

    continuing refactor of css; added previous done snake game content to website; changed initial view of site;

---
+++ src/Controller/SnakeController.php
@@ -0,0 +1,15 @@
+<?php
+// src/Controller/SnakeController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class SnakeController extends AbstractController
+{
+    public function main() : Response
+    {
+        return $this->render('snake/main.html.twig');
+        //return new Response('Snake');
+    }
+}
